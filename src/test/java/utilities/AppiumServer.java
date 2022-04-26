b89ff985102d5a9ee4f9b1811ac6554c8eb8dcee
giangthao <thaogiang.90@gmail.com>
2022-04-26 10:27:28 +0700

    Appium Server

--- src/test/java/utilities/AppiumServer.java
+++ src/test/java/utilities/AppiumServer.java
@@ -5,6 +5,7 @@ import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class AppiumServer {
     public static AppiumDriverLocalService service;
@@ -13,9 +14,22 @@ public class AppiumServer {
 
         // starting the Appium server code
 
-        service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+/*        service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
                 .withArgument(GeneralServerFlag.SESSION_OVERRIDE));
+        service.start();*/
+
+        HashMap<String, String> environment = new HashMap<String, String>();
+        environment.put("PATH", "/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home/bin:/Users/giangthao/Library/Android/sdk/tools:/Users/giangthao/Library/Android/sdk/platform-tools:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" + System.getenv("PATH"));
+        environment.put("ANDROID_HOME", "/Users/giangthao/Library/Android/sdk");
+        environment.put("JAVA_HOME", "/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home");
+        service= AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                .usingDriverExecutable(new File("/usr/local/bin/node"))
+                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+                .withEnvironment(environment).withLogFile(new File("Server.log")));
         service.start();
+
+
 
     }
 
